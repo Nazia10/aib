8ae3caa058f9d875b8323404fe3c4a1ae0bf3817
Nazia10 <90052262+Nazia10@users.noreply.github.com>
2022-10-30 17:55:56 +0530

    Update apps.ps1

--- apps.ps1
+++ apps.ps1
@@ -7,15 +7,6 @@ catch {
     $ErrorMessage = $_.Exception.message
     write-host "Error installing chrome: $ErrorMessage"
 }
-write-host "AIB customization Teams installation done."
+write-host "AIB customization chrome installation done."
 
-write-host "AIB customization installing chrome"
-try {
-    Start-Process -filepath "C:\apps\MSTeamsSetup_c_l_.exe" -Wait -ErrorAction Stop 
-      write-host "Teams installed successfully"
-    }
-catch {
-    $ErrorMessage = $_.Exception.message
-    write-host "Error installing Teams: $ErrorMessage"
-}
-write-host "AIB customization Teams installation done."
+

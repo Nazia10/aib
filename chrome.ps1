f73ff292e1211c6912c8f5af2f6852a6efeb2da8
Nazia10 <90052262+Nazia10@users.noreply.github.com>
2022-10-31 19:29:44 +0530

    Create chrome.ps1

---
+++ chrome.ps1
@@ -0,0 +1,10 @@
+write-host "AIB customization installing chrome"
+try {
+    Start-Process -filepath "C:\apps\ChromeSetup" -Wait -ErrorAction Stop 
+      write-host "chrome installed successfully"
+    }
+catch {
+    $ErrorMessage = $_.Exception.message
+    write-host "Error installing chrome: $ErrorMessage"
+}
+write-host "AIB customization chrome installation done."
